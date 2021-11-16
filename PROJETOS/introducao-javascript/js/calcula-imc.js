0ef0268ea5ccde2552047839c666da87c4695378
franciole <franciole.bezerra@gmail.com>
2021-11-16 00:24:39 -0300

    JAVASCRIPT: PROGRAMANDO NA LINGUAGEM DA WEB - Concluído

---
+++ PROJETOS/introducao-javascript/js/calcula-imc.js
@@ -0,0 +1,60 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (let i = 0; i < pacientes.length; i++) {
+  var paciente = pacientes[i];
+
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
+
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
+
+  var imcTd = paciente.querySelector(".info-imc");
+
+  var pesoEhValido = true;
+  var alturaEhValida = true;
+
+  if (!pesoEhValido) {
+    console.log("Peso inválido");
+    imcTd.textContent = "Peso inválido!";
+    pesoEhValido = false;
+    paciente.classList.add("paciente-invalido");
+  }
+
+  if (!alturaEhValida) {
+    console.log("Altura inválida");
+    imcTd.textContent = "Altura inválida!";
+    alturaEhValida = false;
+    paciente.classList.add("paciente-invalido");
+  }
+
+  if (pesoEhValido && alturaEhValida) {
+    var imc = calculaImc(peso, altura);
+    imcTd.textContent = imc;
+  }
+}
+
+function calculaImc(peso, altura) {
+  var imc = 0;
+  imc = peso / (altura * altura);
+  return imc.toFixed(2);
+}
+
+function validaPeso(peso) {
+  if (peso > 0 && peso < 1000) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validaAltura(altura) {
+  if (altura > 0 && altura <= 3.0) {
+    return true;
+  } else {
+    return false;
+  }
+}
