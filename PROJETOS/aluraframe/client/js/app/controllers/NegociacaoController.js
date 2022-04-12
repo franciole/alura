55714573d6e755b3b440391f5369aab2470781df
franciole <franciole.bezerra@gmail.com>
2022-04-12 01:50:00 -0300

    Curso React fundamentos

--- PROJETOS/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ PROJETOS/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,28 +1,55 @@
 class NegociacaoController {
   constructor() {
     let $ = document.querySelector.bind(document);
+
     this._inputData = $("#data");
     this._inputQuantidade = $("#quantidade");
     this._inputValor = $("#valor");
-    this._listaNegociacoes = new ListaNegociacoes();
+    this._ordemAtual = "";
 
-    this._negociacoesView = new NegociacoesView($("#negociacoesView"));
-    this._negociacoesView.update(this._listaNegociacoes);
+    this._listaNegociacoes = new Bind(
+      new ListaNegociacoes(),
+      new NegociacoesView($("#negociacoesView")),
+      "adiciona",
+      "esvazia",
+      "ordena",
+      "inverteOrdem"
+    );
 
-    this._mensagem = new Mensagem();
-    this._mensagemView = new MensagemView($("#mensagemView"));
-    this._mensagemView.update(this._mensagem);
+    this._mensagem = new Bind(
+      new Mensagem(),
+      new MensagemView($("#mensagemView")),
+      "texto"
+    );
   }
 
   adiciona(event) {
     event.preventDefault();
-    this._listaNegociacoes.adiciona(this._criaNegociacao());
-    this._negociacoesView.update(this._listaNegociacoes);
+    try {   
+      this._listaNegociacoes.adiciona(this._criaNegociacao());
+      this._mensagem.texto = "Negociação adicionada com sucesso";
+      this._limpaFormulario();
+    } catch (erro) {
+      this._mensagem.texto = erro;
+    }
+  }
 
-    this._mensagem.texto = "Negociação adicionada com sucesso!";
-    this._mensagemView.update(this._mensagem);
+  importaNegociacoes() {
+    let service = new NegociacaoService();
+    service
+      .obterNegociacoes()
+      .then((negociacoes) =>
+        negociacoes.forEach((negociacao) => {
+          this._listaNegociacoes.adiciona(negociacao);
+          this._mensagem.texto = "Negociações do período importadas";
+        })
+      )
+      .catch((erro) => (this._mensagem.texto = erro));
+  }
 
-    this._limpaFormulario();
+  apaga() {
+    this._listaNegociacoes.esvazia();
+    this._mensagem.texto = "Negociações apagadas com sucesso";
   }
 
   _criaNegociacao() {
@@ -33,14 +60,20 @@ class NegociacaoController {
     );
   }
 
-  // console.log(negociacao);
-  // console.log(DateHelper.dataParaTexto(negociacao._data));
-
   _limpaFormulario() {
     this._inputData.value = "";
     this._inputQuantidade.value = 1;
     this._inputValor.value = 0.0;
-
     this._inputData.focus();
+  }
+
+  ordena(coluna) {
+    // Implemtaar o método aqui
+    if (this._ordemAtual == coluna) {
+      this._listaNegociacoes.inverteOrdem();
+    } else {
+      this._listaNegociacoes.ordena((a, b) => a[coluna] - b[coluna]);
+    }
+    this._ordemAtual = coluna;
   }
 }
--- PROJETOS/aluraframe/client/js/app/views/NegociacoesView.js
+++ PROJETOS/aluraframe/client/js/app/views/NegociacoesView.js
@@ -1,45 +1,44 @@
 class NegociacoesView extends View {
-  constructor(elemento) {
-    super(elemento);
-  }
-
-  template(model) {
-    return `
-    <table class="table table-hover table-bordered">
-                <thead>
+    
+    constructor(elemento) {
+        
+        super(elemento);
+    }
+    
+    template(model) {
+        
+        return `
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th onClick="negociacaoController.ordena('data')">DATA</th>
+                    <th onclick="negociacaoController.ordena('quantidade')">QUANTIDADE</th>
+                    <th onClick="negociacaoController.ordena('valor')">VALOR</th>
+                    <th onClick="negociacaoController.ordena('volume')">VOLUME</th>
+                </tr>
+            </thead>
+        
+            <tbody>
+                ${model.negociacoes.map(n => `
+                    
                     <tr>
-                        <th>DATA</th>
-                        <th>QUANTIDADE</th>
-                        <th>VALOR</th>
-                        <th>VOLUME</th>
+                        <td>${DateHelper.dataParaTexto(n.data)}</td>
+                        <td>${n.quantidade}</td>
+                        <td>${n.valor}</td>
+                        <td>${n.volume}</td>
                     </tr>
-                </thead>
-
-                <tbody>
-                ${model.negociacoes
-                  .map(
-                    (n) =>
-                      `
-                    <tr>
-                        <td>${DateHelper.dataParaTexto(n._data)}</td>
-                        <td>${n._quantidade}</td>
-                        <td>${n._valor}</td>
-                        <td>${n.getVolume()}</td>
-                    </tr>
-                  `
-                  )
-                  .join("")}
+                    
+                `).join('')}                
             </tbody>
+                  
             <tfoot>
-                    <td colspan="3"></td>
-                    <td>
-                      ${model.negociacoes.reduce(
-                        (total, n) => total + n.getVolume(),
-                        0.0
-                      )}
-                    </td>
+                <td colspan="3"></td>
+                <td>
+                    ${model.volumeTotal}
+                </td>
             </tfoot>
-         </table>
+            
+        </table>
         `;
-  }
+    }
 }
